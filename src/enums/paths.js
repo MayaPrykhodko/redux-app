7c20b5872d5640992ac3075fcf00986c72737fdd
MayaPrykhodko <mp13013@ukr.net>
2023-08-29 23:53:47 +0300

    updated paths

--- src/enums/paths.js
+++ src/enums/paths.js
@@ -1,10 +1,10 @@
 const paths = {
-    SIGN_UP: '/sign-up',
-    SIGN_IN: '/sign-in',
-    MAIN: '/',
-    TRIP_DETAILS: '/trip', 
-    BOOKINGS: '/bookings',
-    OTHERS: '*'
-  };
-  
-  export default paths;
+  SIGN_UP: '/react-redux-travel-app/sign-up',
+  SIGN_IN: '/react-redux-travel-app/sign-in',
+  MAIN: '/react-redux-travel-app/',
+  TRIP_DETAILS: '/react-redux-travel-app/trip', 
+  BOOKINGS: '/react-redux-travel-app/bookings',
+  OTHERS: '/react-redux-travel-app/*'
+};
+
+export default paths;
