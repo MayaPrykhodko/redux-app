cad7f49c5041ab471c5a13ea7edb9d85af866fc1
MayaPrykhodko <mp13013@ukr.net>
2023-08-29 17:27:55 +0300

    added media queries

--- src/Components/Blocks/Footer.js
+++ src/Components/Blocks/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
         <footer className="footer">
             <span className="footer__text">
                 from
-                <a className="footer__link" href="https://binary-studio.com">
-                    binary studio
+                <a className="footer__link" href="#">
+                    travel app
                 </a>
                 with
                 <img className="footer__icon" src={heart} alt="heart" />
