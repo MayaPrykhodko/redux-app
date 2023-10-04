8f94f5f5a99c477c830cd88874c349ed56bf1515
MayaPrykhodko <mp13013@ukr.net>
2023-10-04 10:55:27 +0300

    updated client urls

--- client/src/enums/url.js
+++ client/src/enums/url.js
@@ -1,9 +1,9 @@
 const url = {
-    AUTHENTICATED_USER: 'https://redux-app-livid.vercel.app/api/v1/users/authenticated-user',
-    USER_LOGIN: 'https://redux-app-livid.vercel.app/api/v1/auth/sign-in',
-    USER_REGISTER: 'https://redux-app-livid.vercel.app/api/v1/users/sign-up',
-    TRIPS: 'https://redux-app-livid.vercel.app/api/v1/trips/',
-    BOOKINGS: 'https://redux-app-livid.vercel.app/api/v1/bookings/'
+    AUTHENTICATED_USER: 'https://redux-app.onrender.com/api/v1/users/authenticated-user',
+    USER_LOGIN: 'https://redux-app.onrender.com/api/v1/auth/sign-in',
+    USER_REGISTER: 'https://redux-app.onrender.com/api/v1/users/sign-up',
+    TRIPS: 'https://redux-app.onrender.com/api/v1/trips/',
+    BOOKINGS: 'https://redux-app.onrender.com/api/v1/bookings/'
   };
 
   export default url;
